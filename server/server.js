94b76eb63cde30c989c53ee336ebc4b18a282413
alysahil <106840600+alysahil@users.noreply.github.com>
2026-04-30 15:15:35 +0530

    Fix: Update Express wildcard route for Express 5 compatibility

--- server/server.js
+++ server/server.js
@@ -176,7 +176,7 @@ app.get('/api/users', authenticate, async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
